16fba7be5139fe512ef041dfa599fff3eb3f4bde
ChungIl <cnddlf12@naver.com>
2023-09-03 23:31:31 +0900

    []vmware에 1gb 오라클 환경 만들고 retemfo 계정 생성

---
+++ dbconfig.js
@@ -0,0 +1,6 @@
+module.exports =
+    {
+        user : 'retemfo',
+        password: 'retemfo',
+        connectionString: '192.168.243.1:1521/XE'
+    }
---
+++ oracle.js
@@ -0,0 +1,5 @@
+const oracledb = require('oracledb');
+const dbconfig = require('./dbconfig.js');  // db연결정보 파일
+
+
+/* todo vmware에 1gb 오라클 환경 만들고, retemfo 계정 추가*/
