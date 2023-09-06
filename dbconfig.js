6729cb3be93d7c0414fe10564f5bdec0d00cb0a0
ChungIl <cnddlf12@naver.com>
2023-09-06 21:55:30 +0900

    [*]vmware에 1gb 오라클 환경 만들고 retemfo 계정 생성 [*]vmware에 1gb 오라클 환경 만들고 retemfo 계정 재생성 [*]retemfo 계정 연결 확인 []데이터베이스 모델 설계

--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,6 @@
 module.exports =
     {
-        user : 'retemfo',
-        password: 'retemfo',
-        connectionString: '192.168.243.1:1521/XE'
+        user : 'temfo',
+        password: 'temfo',
+        connectionString: '192.168.243.129:1521/XE'
     }
--- oracle.js
+++ oracle.js
@@ -2,3 +2,40 @@ const oracledb = require('oracledb');
 const dbconfig = require('./dbconfig.js');  // db연결정보 파일
 
 /* todo 오라클 환경 완성. retemfo 계정 연결 확인*/
+
+async function main() {
+
+    const testSql = "SELECT * FROM TEST";
+    const options = {
+        resultSet: true,
+        outFormat: oracledb.OUT_FORMAT_OBJECT
+    };
+
+    let conn = null;
+
+    try {
+        oracledb.initOracleClient({libDir: 'C:/Java/instantclient_21_11'});
+
+        // 오라클 접속정보를 이용해서 오라클 연결객체 하나 생성
+        conn = await oracledb.getConnection(dbconfig);
+
+
+        const result = await conn.execute(testSql);
+        // await conn.commit(); // oracle은 기본적으로 auto commit이 아니라서 그렇다.
+
+        console.log(result);
+
+    } catch(ex) {
+        console.log(ex)
+    } finally {
+        if (conn) {
+            try {
+                await conn.close();
+                console.log('오라클 데이터베이스 접속 해제 성공')
+            }catch(ex) {
+                console.error(ex);
+            }
+        }
+    }
+}
+main();
